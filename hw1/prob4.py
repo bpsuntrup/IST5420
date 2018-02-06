912bd439455f4d368e1ccb49032d29833aed67c3
Ben Sunnyboy <bpsuntrup@outlook.com>
2018-02-06 15:46:38 -0600

    Polish up

--- hw1/prob4.py
+++ hw1/prob4.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3.5
 P_0 = 1000
 t = 5
-j = 0.015
 
 # iterate over values of m, and print corresponding P_t
-for m in range(1,100):
-    P_t = P_0*(1+j/m)**(t*m)
-    print("{} {}".format(m,P_t))
+for j in [.015,.025,.05,.075,.1]:
+    for m in [2,4,6,12]:
+        P_t = P_0*(1+j/m)**(t*m)
+        print("j={}, m={} => P_t={}".format(j,m,P_t))
